d41dcbfd549da71eeb7fcb931fcea659b77ec1a0
BriceNogue <bricenogue@gmail.com>
2026-01-15 17:28:24 +0100

    Déploiement

---
+++ script/DeploySimpleVoting.s.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.26;
+
+import "forge-std/Script.sol";
+import {VoteNFT} from "../src/VoteNFT.sol";
+import {SimpleVotingSystem} from "../src/SimpleVotingSystem.sol";
+
+contract DeploySimpleVoting is Script {
+    function run() external returns (SimpleVotingSystem) {
+        //start and stop braodcast indicates that everything inside means that we are going to call a RPC Node
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+
+        vm.startBroadcast(deployerPrivateKey);
+
+        VoteNFT voteNft = new VoteNFT(msg.sender);
+        SimpleVotingSystem simpleVotingSystem = new SimpleVotingSystem();
+        voteNft.grantRole(voteNft.MINTER_ROLE(), address(voting));
+
+        vm.stopBroadcast();
+        
+        return simpleVotingSystem;
+    }
+}
